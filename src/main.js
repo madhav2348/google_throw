daae0a4b7aa13e080b9a0e079db0c344b8168155
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-04-21 21:06:43 +0530

    trying to add database which is Rxdb

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow , ipcMain} = require("electron");
 const path = require("node:path");
+const { electron } = require("node:process");
 
+const { exposeIpcMainRxStorage } = require("rxdb/plugins/electron");
+const { getRxStorageMemory } = require("rxdb/plugins/storage-memory");
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration:true
     },
   });
 
   //   win.load( path."app/index.html");
   win.loadFile(path.join(__dirname, "..", "app", "index.html"));
-  win.removeMenu()
+  win.removeMenu();
 }
 
 app
@@ -31,4 +35,12 @@ app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
+});
+
+app.on("ready", async () => {
+  exposeIpcMainRxStorage({
+    key: "main-storage",
+    storage: getRxStorageMemory(),
+    ipcMain: ipcMain,
+  });
 });
--- src/render.js
+++ src/render.js
@@ -1,44 +1,49 @@
-// let dialogBox = document
-//   .getElementById("note")
-//   .addEventListener("click", () => {
-//     document.createElement("dialog").dialog("open");
+const { createRxDatabase } = require("rxdb");
+const { getRxStorageIpcRenderer } = require("rxdb/plugins/electron");
+
+
+
+
+// let searchBar = document.querySelector("search");
+
+// let imagesearch = document.getElementById("input-search");
+// let inputtext = document.getElementById("input-text");
+
+// if (imagesearch && inputtext) {
+//   imagesearch.addEventListener("click", () => {
+//     console.log("clicked");
+//     inputtext.focus();
 //   });
+// }
 
-let searchBar = document.querySelector("search");
+// let sidebar = document.querySelector(".sidebar");
+// let sidebarItemsMenu = document.querySelector(".sidebar-items");
 
-// searchBar.addEventListener("click", () => {
-//   searchBar.classList.toggle("clicked");
+// sidebar.addEventListener("click", () => {
+//   sidebarItemsMenu.classList.toggle("activate");
 // });
 
-let imagesearch = document.getElementById("input-search");
-let inputtext = document.getElementById("input-text");
+// let takenote = document.querySelector(".takenote");
+// let titlenote = document.querySelector(".title");
+// takenote.addEventListener("focus", () => {
+//   titlenote.classList.toggle("focused");
+// });
 
-if (imagesearch && inputtext) {
-  imagesearch.addEventListener("click", () => {
-    console.log("clicked");
-    inputtext.focus();
-  });
-}
+// takenote.addEventListener("change", (event) => {
+//   console.log(event.target.value);
+// });
 
-let sidebar = document.querySelector(".sidebar");
-let sidebarItemsMenu = document.querySelector(".sidebar-items");
 
-sidebar.addEventListener("click", () => {
-  sidebarItemsMenu.classList.toggle("activate");
-});
 
-let takenote = document.querySelector(".takenote");
-let titlenote = document.querySelector(".title");
-takenote.addEventListener("focus", () => {
-  titlenote.classList.toggle("focused");
-});
+// titlenote.addEventListener("change", (event) => {
+//   console.log(event.target.value);
+// });
+// let sidebarItem = document.querySelectorAll(".items");
 
-let sidebarItem = document.querySelectorAll(".items");
-
-sidebarItem.addEventListener("click", () => {
-  console.log('clicked')
-  sidebarItem.classList.toggle("activate");
-});
+// sidebarItem.addEventListener("click", () => {
+//   console.log("clicked");
+//   sidebarItem.classList.toggle("activate");
+// });
 
 // takenote.addEventListener("blur", (event) => {
 //   if (event.relatedTarget !== titlenote) {
@@ -48,3 +53,20 @@ sidebarItem.addEventListener("click", () => {
 // takenote.addEventListener('input',()=>{
 //   titlenote.classList.toggle('focused')
 // })
+
+// let formData = document.querySelector(".form");
+// formData.addEventListener("change", (event) => {
+//   console.log(event.target.value);
+// });
+
+
+const db = await createRxDatabase({
+  name: "google_throw",
+  storage: getRxStorageIpcRenderer({
+    key: "main-storage",
+    ipcRenderer: electron.ipcRenderer,
+  }),
+});
+
+
+export default db;
