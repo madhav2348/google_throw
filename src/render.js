8d2ec40f9c3e5df69636dadec1277427a543447b
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-04-21 19:33:20 +0530

    making it responsive

--- src/render.js
+++ src/render.js
@@ -4,28 +4,47 @@
 //     document.createElement("dialog").dialog("open");
 //   });
 
-// let searchBar = document.querySelector("search");
+let searchBar = document.querySelector("search");
 
 // searchBar.addEventListener("click", () => {
 //   searchBar.classList.toggle("clicked");
 // });
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   let imagesearch = document.getElementById("input-search");
-//   let inputtext = document.getElementById("input-text");
+let imagesearch = document.getElementById("input-search");
+let inputtext = document.getElementById("input-text");
 
-//   if (imagesearch && inputtext) {
-//     imagesearch.addEventListener("click", () => {
-//       inputtext.focus();
-//     });
-//   }
-// });
+if (imagesearch && inputtext) {
+  imagesearch.addEventListener("click", () => {
+    console.log("clicked");
+    inputtext.focus();
+  });
+}
 
 let sidebar = document.querySelector(".sidebar");
 let sidebarItemsMenu = document.querySelector(".sidebar-items");
 
 sidebar.addEventListener("click", () => {
-  sidebarItemsMenu.classList.toggle('activate')
+  sidebarItemsMenu.classList.toggle("activate");
 });
 
+let takenote = document.querySelector(".takenote");
+let titlenote = document.querySelector(".title");
+takenote.addEventListener("focus", () => {
+  titlenote.classList.toggle("focused");
+});
 
+let sidebarItem = document.querySelectorAll(".items");
+
+sidebarItem.addEventListener("click", () => {
+  console.log('clicked')
+  sidebarItem.classList.toggle("activate");
+});
+
+// takenote.addEventListener("blur", (event) => {
+//   if (event.relatedTarget !== titlenote) {
+//     titlenote.classList.remove("focused");
+//   }
+// });
+// takenote.addEventListener('input',()=>{
+//   titlenote.classList.toggle('focused')
+// })
